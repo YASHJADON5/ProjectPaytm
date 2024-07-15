eb42a14c34e9dae649af3c154df9c2298d2bb136
YASHJADON5 <jadonyash2@gmail.com>
2024-07-15 21:25:45 +0530

    add money feature added

--- projectPAYTM/frontend/src/assets/Base_Url.js
+++ projectPAYTM/frontend/src/assets/Base_Url.js
@@ -1,2 +1,2 @@
-// export const Base_Url ="https://projectpaytm-backend.onrender.com"
-export const Base_Url ="http://localhost:5242"
+export const Base_Url ="https://projectpaytm-backend.onrender.com"
+// export const Base_Url ="http://localhost:5242"
